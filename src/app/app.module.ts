5d04365d85ef9ca3f1840a431de8a404fb26db9b
Shashikanth M S <shashikanthms1998@gmail.com>
2023-02-14 20:28:30 +0530

    form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormcComponent } from './component/formc/formc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormcComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/formc/formc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formc',
+  templateUrl: './formc.component.html',
+  styleUrls: ['./formc.component.css']
+})
+export class FormcComponent {
+
+}
